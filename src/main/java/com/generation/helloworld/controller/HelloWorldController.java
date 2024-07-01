5ea6a3660d104229838c707b8fd16818f27c2127
Wil220 <wilker.silva0714@gmail.com>
2024-07-01 13:31:06 -0300

    objetivos

--- src/main/java/com/generation/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/generation/helloworld/controller/HelloWorldController.java
@@ -5,11 +5,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello-world")
+@RequestMapping("/")
 public class HelloWorldController{
 
-	@GetMapping
+	@GetMapping ("/hello-wolrd")
 	public String helloworld() {
 		return "Hello World";
 	}
+	@GetMapping ("/lista-bdsm") 
+	public String listaBdsm() {
+		return "orientação ao futuro, responsabilidade pessoal, mentalidade de crescimento, persistencia, trabalho em equipe, orientação ao detalhe, proatividade, comunicação";
+	}
+	@GetMapping ("/objetivos")
+	public String objeivos() {
+		return "Focar em oritação ao detalhe, Praticar, Estudar ";
+	}
+	
 }
